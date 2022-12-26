ccbff499e0fc1eaa9724050043aa238b6af8cf31
imyuanli <2865437316@qq.com>
2022-12-26 23:06:49 +0800

    tools

---
+++ src/components/tools.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="absolute-center top-56">
+    <div class="grid gap-5 grid-cols-5 justify-items-center grid-rows-4">
+      <div class="flex-center flex-col">
+        <div class="flex-center mb-1 customNav">
+          <el-icon class="icon-plus mb-3" style="">
+            <Plus/>
+          </el-icon>
+
+        </div>
+        <div class="text-white text-base overflow-ellipsis truncate ">1232123212321232</div>
+      </div>
+
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+      <div class="flex-center flex-col customNav">
+        <el-icon class="icon-plus" style="">
+          <Plus/>
+        </el-icon>
+      </div>
+    </div>
+    <!--    <div class="item">-->
+    <!--      <div>-->
+    <!--        <div v-if="items.iconObj.isText">-->
+    <!--          123-->
+    <!--        </div>-->
+    <!--        <div v-if="!items.iconObj.isText">-->
+    <!--          <img class="edit-icon" :src="items.iconObj.data" alt="">-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--      <span class="item-title">{{ items.title }}</span>-->
+    <!--    </div>-->
+  </div>
+</template>
+
+<script setup>
+const shortcuts_list = [
+  {
+    value: '选项1',
+    label: '黄金糕'
+  }, {
+    value: '选项2',
+    label: '双皮奶',
+    disabled: true
+  }, {
+    value: '选项3',
+    label: '蚵仔煎'
+  }, {
+    value: '选项4',
+    label: '龙须面'
+  }, {
+    value: '选项5',
+    label: '北京烤鸭'
+  }]
+</script>
+
+<style scoped>
+.customNav {
+  width: 80px;
+  height: 80px;
+  border-radius: 15px;
+  text-align: center;
+  background-color: rgba(255, 255, 255, .1);
+  cursor: pointer;
+  transition: background-color .35s;
+  backdrop-filter: blur(40px) saturate(1.5);
+}
+
+.icon-plus {
+  color: rgba(255, 255, 255, .5);
+  font-size: 32px;
+}
+
+
+.customNav:hover {
+  background: rgba(255, 255, 255, .5);
+}
+
+.customNav:hover .icon-plus {
+  color: rgba(0, 0, 0, .5);
+}
+
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="content" @click.self="handleClickClose" @contextmenu.prevent.stop="handleInputHidden">
+  <div class="content" @click.self="handleClickClose" @contextmenu.prevent.stop="">
     <!--    背景图片-->
     <img
-       src="https://tva4.sinaimg.cn/large/0060lm7Tly1ftg6xc454vj31hc0u07wh.jpg"
+        src="https://tva4.sinaimg.cn/large/0060lm7Tly1ftg6xc454vj31hc0u07wh.jpg"
         alt="背景"
         :class="['fixed top-0 left-0 object-cover -z-30 w-full h-full duration-100',isTwoPage || isFocus?'scale-110 blur':'']"
     >
     <!--    背景颜色-->
     <div class="cover"/>
     <!--    时间-->
-    <div
-        class="absolute left-1/2 -translate-x-1/2 top-24 text-4xl font-semibold cursor-pointer hover:scale-125 duration-200 text-white"
-        @click="handleInputHidden"
-    >
+    <div class="absolute-center top-24 text-4xl font-semibold cursor-pointer hover:scale-125 duration-200 text-white">
       11:22
     </div>
     <!--搜索框-->
-    <div v-show="!isTwoPage">
-      <Search
-          :isFocus="isFocus"
-          :handleClickOpen="handleClickOpen"
-      />
-    </div>
-    <!--    二级页面-->
-    <div v-show="isTwoPage">
-      123123
-    </div>
+    <Search
+        :isFocus="isFocus"
+        :handleClickOpen="handleClickOpen"
+    />
+
+    <!--    快捷方式-->
+    <Tools/>
+    <!--    <div v-show="!isTwoPage">-->
+    <!--     -->
+    <!--    </div>-->
+    <!--    &lt;!&ndash;    二级页面&ndash;&gt;-->
+    <!--    <div v-show="isTwoPage">-->
+    <!--      123123-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
 import Search from '../components/search.vue'
+import Tools from '../components/tools.vue'
 //输入框的值
 const inputValue = ref(null)
 //输入框聚焦
@@ -41,18 +43,17 @@ const handleClickOpen = () => {
 }
 const handleClickClose = () => {
   isFocus.value = false
-  handleInputBlock()
 }
 
 //二级也页面
-const isTwoPage = ref(false)
+// const isTwoPage = ref(false)
 //鼠标右键
-const handleInputHidden = () => {
-  isTwoPage.value = false
-}
-const handleInputBlock= () => {
-  isTwoPage.value = true
-}
+// const handleInputHidden = () => {
+//   isTwoPage.value = false
+// }
+// const handleInputBlock= () => {
+//   isTwoPage.value = true
+// }
 
 </script>
 
@@ -65,7 +66,7 @@ const handleInputBlock= () => {
 
 .cover {
   z-index: -10;
-  opacity: 0;
+  opacity: 1;
   position: fixed;
   left: 0;
   top: 0;
