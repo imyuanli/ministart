1e0604ed045893605efdb6bb5d383b47bf8e22db
yuanli <2865437316@qq.com>
2022-12-26 17:32:36 +0800

    element ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index.js";
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router).use(ElementPlus, {locale}).mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
