64961f76aa34c7fb54a8cc4c30b36866516d8702
yuanli <2865437316@qq.com>
2022-12-26 17:33:45 +0800

    首页代码

---
+++ src/pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="content" @click.self="handleClickClose">
+    <!--    背景图片-->
+    <img
+        @click.self="handleClickClose" src="https://tva4.sinaimg.cn/large/0060lm7Tly1ftg6xc454vj31hc0u07wh.jpg"
+        alt="背景"
+        :class="['fixed top-0 left-0 object-cover -z-30 w-full h-full duration-100',isFocus?'scale-110 blur-md':'']"
+    >
+    <!--    背景颜色-->
+    <div class="cover"/>
+    <!--    时间-->
+    <div
+        class="absolute left-1/2 -translate-x-1/2 top-24 text-4xl font-semibold cursor-pointer hover:scale-125 duration-200 text-white">
+      11:22
+    </div>
+    <!--搜索框-->
+    <Search
+        :isFocus="isFocus"
+        :handleClickOpen="handleClickOpen"
+    />
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import Search from '../components/search.vue'
+//输入框的值
+const inputValue = ref(null)
+//输入框聚焦
+const isFocus = ref(false)
+const handleClickOpen = () => {
+  isFocus.value = true
+}
+const handleClickClose = () => {
+  isFocus.value = false
+}
+</script>
+
+<style scoped>
+.content {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.cover {
+  z-index: -10;
+  opacity: 0;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-image: radial-gradient(rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, .5) 100%), radial-gradient(rgba(0, 0, 0, 0) 33%, rgba(0, 0, 0, .3) 166%);
+  transition: .25s;
+}
+</style>
