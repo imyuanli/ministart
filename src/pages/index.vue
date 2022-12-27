7e2f8c4cc5eca06634705d9c8b5edab014b67da4
imyuanli <2865437316@qq.com>
2022-12-27 21:48:19 +0800

    首页调整

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="content" @click.self="handleClickClose" @contextmenu.prevent.stop="">
+  <div class="relative w-full h-screen min-h-screen">
     <img
         src="https://tva4.sinaimg.cn/large/0060lm7Tly1ftg6xc454vj31hc0u07wh.jpg"
         alt="背景"
-        :class="['fixed top-0 left-0 object-cover -z-30 w-full h-full duration-100',isTwoPage || isFocus?'scale-110 blur':'']"
+        :class="['fixed top-0 left-0 object-cover -z-30 w-full h-full duration-100 opacity-0']"
     >
     <div class="cover"/>
     <NavBar/>
-    <!--主要内容-->
-    <div class="main" @click.self="handleClickClose">
-      <!--    时间-->
-      <div
-          class="absolute left-1/2 -translate-x-1/2 top-24 text-4xl font-semibold cursor-pointer hover:scale-125 duration-200 text-white">
-        11:22
+    <div class="main">
+      <div class="absolute left-1/2 -translate-x-1/2 w-full flex-center top-32 flex-col">
+        <!--        有背景时间就变成白色-->
+        <div
+            class="text-4xl font-semibold cursor-pointer hover:scale-125 duration-200 mb-6">
+          11:22
+        </div>
+        <MySearch/>
       </div>
-      <!--搜索框-->
-      <MySearch :isFocus="isFocus" :handleClickOpen="handleClickOpen"/>
-      <!--    快捷方式-->
-      <MyTools/>
     </div>
-    <MyFooter/>
+
+    <!--主要内容-->
+    <!--    <div class="main" @click.self="handleClickClose">-->
+    <!--      &lt;!&ndash;    时间&ndash;&gt;-->
+
+    <!--      &lt;!&ndash;搜索框&ndash;&gt;-->
+
+    <!--      &lt;!&ndash;    快捷方式&ndash;&gt;-->
+    <!--      <MyTools/>-->
+    <!--    </div>-->
+    <!--    <MyFooter/>-->
   </div>
 </template>
 
@@ -31,14 +39,6 @@ import NavBar from '../components/nav-bar.vue'
 import MyFooter from "../components/my-footer.vue";
 //输入框的值
 const inputValue = ref(null)
-//输入框聚焦
-const isFocus = ref(false)
-const handleClickOpen = () => {
-  isFocus.value = true
-}
-const handleClickClose = () => {
-  isFocus.value = false
-}
 
 //常规设置
 
@@ -73,17 +73,18 @@ const settingWord = {
   width: 100%;
   height: 100vh;
   min-height: 100vh;
+  background: #f5f5f5;
 }
 
 .cover {
   z-index: -10;
-  opacity: 1;
+  opacity:1;
   position: fixed;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  background-image: radial-gradient(rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, .5) 100%), radial-gradient(rgba(0, 0, 0, 0) 33%, rgba(0, 0, 0, .3) 166%);
+  background: #f5f5f5;
   transition: .25s;
 }
 
