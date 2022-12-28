292dd06204f3ac08af1125ce1de14fc37a4d7da5
yuanli <2865437316@qq.com>
2022-12-28 17:34:24 +0800

    下载dayjs

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
-
 const app = createApp(App)
 app.use(router).use(ElementPlus, {locale}).mount('#app')
+
+//element icon
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
