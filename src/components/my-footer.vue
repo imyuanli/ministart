2ea20df7918bb58ddcff24f2f603e60b564ac44e
yuanli <2865437316@qq.com>
2022-12-29 12:54:45 +0800

    footer重置

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="['footer',textColor]">
+  <div class="footer">
     <div class="mb-1">
-      Copyright © 2022 鸢离
+      Copyright © {{ new Date().getFullYear() }} 鸢离
     </div>
     <div>
       <a class="text" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41052202001272">豫公安网备41052202001272</a>
@@ -11,25 +11,21 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  textColor: String
-})
+
 </script>
 
 <style scoped>
 .footer {
   display: inline-block;
-  border-radius: 2px;
   backdrop-filter: blur(10px);
   animation: delayedFadeIn 1s;
   position: fixed;
   bottom: 10px;
   left: 50%;
   transform: translateX(-50%);
-  width: fit-content;
   font-size: 12px;
   text-align: center;
-  text-shadow: 0 0 20px rgb(0 0 0 / 80%);
+  text-shadow: 0 0 20px rgb(0 0 0 / 40%);
   white-space: nowrap;
   padding: 3px 10px;
 }
