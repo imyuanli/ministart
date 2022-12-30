74d1bf2a260a9859450354efa0014d4ee2696561
imyuanli <2865437316@qq.com>
2022-12-31 00:42:50 +0800

    设置组件封装

--- src/components/setting-panel.vue
+++ src/components/setting-panel.vue
@@ -1,15 +1,59 @@
 <template>
- <div class="mt-3">
+ <div class="mb-3">
    <div class="ml-2 font-bold text-black mb-1">
      <slot name="panel-title">panel-title</slot>
    </div>
    <div class="bg-white rounded-md px-6 py-3">
-     <slot name="panel-content"></slot>
+     <div v-for="(item,index) in Object.keys(settingObj)" :key="index">
+       <div class="flex justify-between py-3">
+         <div class="flex-1 flex justify-center items-start flex-col">
+           <div class="text-md text-black">
+             {{ setInfo[item].intro }}
+           </div>
+           <div class="text-xs text-gray-400">
+             {{ setInfo[item]?.tips }}
+           </div>
+         </div>
+         <div class="flex-1 flex justify-end items-center">
+           <el-switch
+               v-if="setInfo[item].type==='switch'"
+               v-model="settingObj[item]"
+           />
+           <el-slider
+               v-if="setInfo[item].type==='slider'"
+               v-model="settingObj[item]"
+               class="w-full"
+               :min="setInfo[item].min"
+               :max="setInfo[item].max"
+           />
+           <el-select
+               v-if="setInfo[item].type==='select'"
+               v-model="settingObj[item]"
+           >
+             <el-option
+                 v-for="item in setInfo[item].options"
+                 :key="item.value"
+                 :label="item.label"
+                 :value="item.value"
+             />
+           </el-select>
+           <el-color-picker
+               v-if="setInfo[item].type==='picker'"
+               v-model="settingObj[item]"
+           />
+         </div>
+       </div>
+     </div>
    </div>
  </div>
 </template>
 
-<script>
+<script setup>
+import {setInfo} from '../utils/index.js'
+defineProps({
+  settingObj:Object
+})
+
 </script>
 
 <style scoped>
