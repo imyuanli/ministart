0e2e68530029efa63a32f5ba22034b8be661c95f
imyuanli <2865437316@qq.com>
2022-12-31 00:03:17 +0800

    mytime组件封装

---
+++ src/components/my-time.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="time" class="absolute left-1/2 -translate-x-1/2 top-24 flex-center flex-col">
+    <div
+        class="cursor-pointer hover:scale-125 duration-200"
+        :style="{
+              fontSize:size+'px',
+              fontWeight:weight?400:600,
+              fontFamily:family,
+              color:color,
+            }"
+    >
+      {{ dayjs(new Date()).format("HH:mm") }}
+    </div>
+    <div>
+      {{ dayjs(new Date()).format("MM-DD") }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {toRefs} from "vue";
+import dayjs from "dayjs"
+
+const props = defineProps({
+  timeSetting: Object
+})
+
+const {
+  time,
+  size,
+  weight,
+  family,
+  color,
+  month,
+  week,
+  lunar,
+} = toRefs(props.timeSetting)
+</script>
+
+<style scoped>
+
+</style>
