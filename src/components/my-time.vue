a047c81044947fd2547be77758f4fb00d38a4a83
imyuanli <2865437316@qq.com>
2022-12-31 00:42:33 +0800

    日期显示逻辑

--- src/components/my-time.vue
+++ src/components/my-time.vue
@@ -9,17 +9,20 @@
               color:color,
             }"
     >
-      {{ dayjs(new Date()).format("HH:mm") }}
+      {{ nowTime }}
     </div>
-    <div>
-      {{ dayjs(new Date()).format("MM-DD") }}
+    <div class="text-white">
+      <span v-show="month" class="m-1">{{ nowMonth }}</span>
+      <span v-show="week" class="m-1">{{ nowWeek }}</span>
+      <span v-show="lunar" class="m-1">{{ nowLunar }}</span>
     </div>
   </div>
 </template>
 
 <script setup>
-import {toRefs} from "vue";
+import {onBeforeUnmount, onMounted, ref, toRefs} from "vue";
 import dayjs from "dayjs"
+import solarLunar from 'solarlunar-es';
 
 const props = defineProps({
   timeSetting: Object
@@ -35,6 +38,37 @@ const {
   week,
   lunar,
 } = toRefs(props.timeSetting)
+
+const nowTime = ref()
+const nowMonth = ref()
+const nowWeek = ref()
+const nowLunar = ref()
+const getNowTime = () => {
+  if (time) {
+    const d = dayjs()
+    //时间
+    nowTime.value = d.format("HH:mm")
+    nowMonth.value = d.format("MM[月]DD[日]")
+    //星期加农历
+    const {ncWeek, monthCn, dayCn} = solarLunar.solar2lunar(d.year(), d.month() + 1, d.date())
+    nowWeek.value = ncWeek
+    nowLunar.value = monthCn + dayCn
+  }
+}
+//定义 timer 初始值及类型
+let timer = null
+onMounted(() => {
+  getNowTime()
+  timer = setInterval(() => {
+    getNowTime()
+  }, 1000)
+})
+
+onBeforeUnmount(() => {
+  // 清理定时器要处理 timer 的类型
+  clearInterval(Number(timer))
+})
+
 </script>
 
 <style scoped>
