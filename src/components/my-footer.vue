11376e8792c3b29b510875a2c8adcc7751902572
imyuanli <2865437316@qq.com>
2022-12-31 00:43:11 +0800

    footer组件

--- src/components/my-footer.vue
+++ src/components/my-footer.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="footer">
-    <div class="mb-1">
+    <div>
       Copyright © {{ new Date().getFullYear() }} 鸢离
+    </div>
+    <div v-if="simple"  class="mt-1">
+      <a class="text" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41052202001272">豫公安网备41052202001272</a>
+      <el-divider direction="vertical"/>
+      <a class="text" href="https://beian.miit.gov.cn/">冀ICP备2020030880号</a>
     </div>
   </div>
 </template>
 
 <script setup>
-
+defineProps({
+  simple: Boolean
+})
 </script>
 
 <style scoped>
 .footer {
   display: inline-block;
-  backdrop-filter: blur(10px);
+  backdrop-filter: blur(8px);
   animation: delayedFadeIn 1s;
   position: fixed;
   bottom: 10px;
@@ -23,6 +30,7 @@
   text-align: center;
   text-shadow: 0 0 20px rgb(0 0 0 / 40%);
   white-space: nowrap;
-  padding: 3px 10px;
+  padding: 3px 5px;
+  color:#ffffff;
 }
 </style>
