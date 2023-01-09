6faae2790582fd14d8a1115be640b73eec9f8e83
imyuanli <2865437316@qq.com>
2023-01-09 21:26:26 +0800

    BASE_URL

--- src/service/service.js
+++ src/service/service.js
@@ -2,7 +2,7 @@ import request from '../service/request'
 
 // export const BASE_URL = 'http://127.0.0.1:8000/mini'
 
-const BASE_URL = 'https://imyuanli.cn/api/mini'
+const BASE_URL = 'https://api.imyuanli.cn/api/mini'
 
 export async function get_url_icon(payload) {
     return request.post(BASE_URL + '/get_url_icon/', payload)
