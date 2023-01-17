861cfc9bba38785f144c34890bfee8a260822f82
yuanli <2865437316@qq.com>
2023-01-17 16:14:59 +0800

    test drag

---
+++ src/components/nested.vue
@@ -0,0 +1,26 @@
+<template>
+  <draggable
+      class="wrapper"
+      :list="list"
+      item-key="index"
+  >
+    <template #item="{element, index}">
+      <p>{{element.name}}</p>
+    </template>
+  </draggable>
+</template>
+<script setup>
+import draggable from "vuedraggable";
+
+defineProps({
+  list: Array
+})
+</script>
+<style scoped>
+p {
+  margin: 5px;
+  padding: 10px;
+  height: 20px;
+  background: lightcoral;
+}
+</style>
